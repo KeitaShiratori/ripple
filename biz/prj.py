e23d2938a3dde30059b77da959b268eaa15642d9
CHARA-BLADE\keita <keita.shiratori@gmail.com>
2020-05-09 23:07:41 +0900

    API接続処理をbizに移管

--- biz/prj.py
+++ biz/prj.py
@@ -1,15 +1,11 @@
 import requests
-import pprint
+import os
 
-base_url = 
+url_items = os.environ.get('API_ENDPOINT')
+headers = {
+  'x-api-key':os.environ.get('API_KEY')
+}
 
-item_id = r_post.json()['id']
-print(item_id)
-# 93ead2568150009de5f1
-
-r_get = requests.get(url_items + '/' + item_id, headers=headers)
-
-print(r_get.status_code)
-# 200
-
-pprint.pprint(r_get.json())
+def get(prj_id):
+  r_get = requests.get(url_items + '/prj/' + prj_id, headers=headers)
+  return r_get.json()
