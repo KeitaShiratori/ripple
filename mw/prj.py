7f35778fe52114e634059565c1988290884b5da4
CHARA-BLADE\keita <keita.shiratori@gmail.com>
2020-05-17 23:54:23 +0900

    修正

--- mw/prj.py
+++ mw/prj.py
@@ -27,7 +27,7 @@ def create_prj(data):
   # 入力されたprj情報にprj_idと生成日時を付与する
   data['create_date'] = datetime.now().isoformat()
   data['prj_id'] = _make_prj_id(data['create_date'])
-  data['user_id' = data['fbid']
+  data['user_id'] = data['fbid']
   data['user_name'] = data['fbnm']
   data['member_type'] = 'leader'
   del data['fbid'], data['fbnm']
